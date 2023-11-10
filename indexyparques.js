cc5c4c3321f83bb0964bb9dc9609cb4ebe764438
WilmerAgu <105666923+wilmeragudelo@users.noreply.github.com>
2023-11-10 08:23:27 -0500

    agregando footer

--- indexyparques.js
+++ indexyparques.js
@@ -1,14 +1,3 @@
-// cabio de fecha footer
-
-document.addEventListener("DOMContentLoaded", function() {
-    var fecha = new Date();
-    var ano = fecha.getFullYear();
-    var mes = fecha.getMonth() + 1;
-    var dia = fecha.getDate();
-    var fechaActual = dia + '/' + mes + '/' + ano;
-    var elementoFecha = document.getElementById("fecha-actualizacion");
-    elementoFecha.innerHTML = 'Última actualización: ' + fechaActual;
-  });
 
   // carrusel parques
   document.addEventListener("DOMContentLoaded", function () {
